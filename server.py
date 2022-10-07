5f1050a554df3040e39058fc19ecd40ce09d6e79
Dean Pierce <pierce403@gmail.com>
2022-10-07 10:39:59 -0700

    fixing a typo

--- server.py
+++ server.py
@@ -101,7 +101,7 @@ def secret():
 def squatch_check():
   current_user = get_jwt_identity()
   #numtokens = tokencount(current_user)
-  if squath_club(current_user):
+  if squatch_club(current_user):
     msg = "Nice Squatch you got there  FLAG 500: "+os.environ['FLAG500']
   else:
     msg = "no squatches!"
